50f829a403c8714b77c285557228204bf8b62fcb
jeoliveira045 <jean.mateus.1997@gmail.com>
2022-08-16 22:30:59 -0300

    Who?

--- src/model/application/Main.java
+++ src/model/application/Main.java
@@ -1,8 +1,5 @@
 public class Main{
     public static void main(String[] args){
-        Integer x = 10;
-        Integer y = 20;
-        Integer z = x + y;
-        System.out.println(z);
+        System.out.println("Who was in paris?");
     }
 }
